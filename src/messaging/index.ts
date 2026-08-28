0664daf6bbcbbeaeadaebe803f1125a3fe0586ea
Usama Imran <uusamaimran@gmail.com>
2026-08-28 15:55:44 +0500

    Update index.ts

--- src/messaging/index.ts
+++ src/messaging/index.ts
@@ -1,5 +1,7 @@
 import { documentUploadConsumer } from "./consumers/documentConsumer.js";
 
 export async function startConsumers(): Promise<void> {
-  await Promise.all([documentUploadConsumer.consume("document.uploaded")]);
+  await Promise.all([
+    documentUploadConsumer.consume("document.uploaded", { useDLQ: true }),
+  ]);
 }
